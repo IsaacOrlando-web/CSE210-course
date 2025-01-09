cb01dced7184acdf72fb993a7e5cfe3b4fc98e05
IsaacOrlando-web <isaacserviciosweb@gmail.com>
2025-01-09 14:51:38 -0300

    Excersice 03 completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        // The random number
+        Random random = new Random();
+        int number = random.Next(1, 100);
+        string userInput = "";
+        int userNumber;
+        int triesnumber = 0;
+
+        Console.WriteLine("What is the magic number? " + number);
+
+        do
+        {
+            Console.Write("What is your guess?");
+            userInput = Console.ReadLine();
+            userNumber = int.Parse(userInput);
+            if(userNumber == number){
+                Console.WriteLine("You guessed it!");
+            } 
+            else if (userNumber < number){
+                Console.WriteLine("Higher");
+            } else {
+                Console.WriteLine("Lower");
+            }
+
+            triesnumber++;
+        }while (userNumber != number);
+
+        //show the number of tries
+        Console.WriteLine($"Number of tries: {triesnumber}");
+        
     }
 }
