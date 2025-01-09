a835f8401bfd77da3775c2cd9819a16d60097744
IsaacOrlando-web <isaacserviciosweb@gmail.com>
2025-01-09 12:04:22 -0300

    finish Activity 01

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!!! i'm Isaac");
+        Console.WriteLine("This is in c#.");
+
+        Console.WriteLine("What is your favorite color? ");
+        String color = Console.ReadLine();
+        Console.WriteLine($"Your favorite color is {color}");
     }
 }
--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        Console.Write("What is your favorite first name? ");
+        String fname = Console.ReadLine();
+        
+        Console.Write("What is your last name? ");
+        String lname = Console.ReadLine();
+
+        Console.WriteLine($"\nYour name is {lname}, {fname} {lname}.");
     }
 }
