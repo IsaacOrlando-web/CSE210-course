acde62d00b4c0b53cefad7491148e08e3212aaa5
IsaacOrlando-web <isaacserviciosweb@gmail.com>
2025-01-23 22:07:30 -0300

    Reference Class Done

---
+++ week03/ScriptureMemorizer/Reference.cs
@@ -0,0 +1,43 @@
+// Keeps track of the book, chapter, and verse information.
+namespace ScriptureMemorizer
+{
+    public class Reference
+    {
+        private string _book;
+        private int _chapter;
+        private int _verse;
+        private int _endVerse;
+
+        //Constructors
+        public Reference(string book, int chapter, int verse)
+        {
+            _book = book;
+            _chapter = chapter;
+            _verse = verse;
+            _endVerse = verse;
+        }
+
+        public Reference(string book, int chapter, int startVerse, int endVerse)
+        {
+            _book = book;
+            _chapter = chapter;
+            _verse = startVerse;
+            _endVerse = endVerse;
+        }
+
+        //Methods
+        public string GetDisplayText()
+        {
+            if (_verse == _endVerse)
+            {
+                return $"{_book} {_chapter}:{_verse}";
+            }
+            else
+            {
+                return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+            }
+        }//The Reference class is pretty simple as far as behaviors go. It should have the ability to get the display text of the reference, which is just a string combining the book, chapter, and verse (or verses). You could consider having getters and setters for each of the data elements that this class stores, but it may be even better to use a constructor to set them.
+
+
+    }
+}
